d1d8d539ae5fcca71bcaa3a0eda6e967ac52e152
project-noon <juniorranger6@gmail.com>
2025-06-07 11:59:26 -0700

    added tower placing and began navigation

---
+++ Player.gd
@@ -0,0 +1,36 @@
+extends Camera3D
+
+var mouseposition
+@onready var raycast:=$RayCast3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#print(mouseposition)
+	#var project = project_position(mouseposition,1.0)
+	#var direction = (project - position).normalized()
+	var direction = project_local_ray_normal(mouseposition)
+	raycast.target_position=direction*50
+	if raycast.is_colliding():
+		if Input.is_action_just_pressed("click"):
+			var tower = preload("res://Base_tower.tscn").instantiate()
+			tower.global_position = raycast.get_collision_point()
+			var collider = raycast.get_collider()
+			var parent = collider.get_parent()
+			
+			if parent is NavigationRegion3D:
+				#parent.bake_navigation_mesh(false)
+				collider.get_parent().add_child(tower)
+				
+	#var dropPlane = Plane(Vector3(0,1,0))
+	
+	
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouseposition = event.position
---
+++ base_tower.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@onready var timer: = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	if get_parent() is NavigationRegion3D:
+		print("baking")
+		#get_parent().bake_navigation_mesh(true)
